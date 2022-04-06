6d0550e7dad0f6611fc2cbb9c895918b6ea781dc
Higgs32584 <mjhiggins2@loyola.edu>
2022-04-06 16:57:36 -0400

    stuff

--- Higgins.java
+++ Higgins.java
@@ -1,13 +1,44 @@
-import java.io.*;
+import java.util.Scanner;
 import java.io.FileReader;
-
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.ArrayList;
 public class Higgins{
-    public static void main(String[] args){
-		// File path is passed as parameter
-		File file = new File("file1.txt");
-
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String st;
-		while (st = br.readLine() != null){System.out.println(st);}
+	public void full_arr(ArrayList<String> arr1, ArrayList<String> arr2){
+		System.out.println('Yo');
 	}
+
+    public static void main(String[] args){
+	ArrayList<String> arr1= new ArrayList<String>();
+	ArrayList<String> arr2= new ArrayList<String>();
+    try {
+      File myObj = new File("file1.txt");
+      Scanner myReader = new Scanner(myObj);
+	  while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        arr1.add(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
+	
+    try {
+      File myObj2 = new File("file2.txt");
+      Scanner myReader2 = new Scanner(myObj2);
+	  while (myReader2.hasNextLine()) {
+        String data = myReader2.nextLine();
+        arr2.add(data);
+      }
+      myReader2.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+	//Print here!
+	System.out.println(arr1);
+	System.out.println(arr2);
+}
+}
---
+++ python_conv.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+class python_conv{
+arr1=['Spring 2022','Hi','Spring 2022','After Spring break','Masks are optional','Starting March 18, 2022','Until the end of','May']
+arr2=['Hi',
+'This is',
+'Spring 2022',
+'Maybe',
+'After Spring break',
+'Masks are optional',
+'Up To',
+'May',
+'Until the end of the semester',
+'Then it is summer?']
+public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+
+
+}
